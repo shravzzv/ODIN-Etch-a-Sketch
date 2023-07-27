3303ad67d8f96737e40cc2728be06e6e06b11050
sai shravan <saishravan384@gmail.com>
2023-07-27 21:45:16 +0530

    feat: Add grid according to the user's input

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ for (let i = 0; i < 256; i++) {
   box.classList.add('box')
   container.appendChild(box)
 }
+
 const button = document.createElement('button')
 button.innerText = 'set squares'
 document.body.insertBefore(button, container)
@@ -13,3 +14,20 @@ button.addEventListener('click', (e) => {
   const squaresPerSide = parseInt(
     prompt('How many number of squares per side?', 10)
   )
+
+  // remove existing grid
+  const existingBoxes = document.querySelectorAll('.box')
+  existingBoxes.forEach((box) => box.remove())
+
+  for (let i = 0; i < squaresPerSide ** 2; i++) {
+    const box = document.createElement('div')
+    box.classList.add('box')
+    box.style.height = `calc(100% / ${squaresPerSide})`
+    box.style.flexBasis = `calc(100% / ${squaresPerSide})`
+    container.appendChild(box)
+
+    box.addEventListener('mouseover', (e) => {
+      console.dir(e.target)
+    })
+  }
+})
